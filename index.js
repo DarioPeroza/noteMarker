10622d503c2e7950a7622ca11020e7f5d675e69d
Dario Peroza <exskey@gmail.com>
2021-03-08 22:29:15 -0400

    Add function writeTaskCard

--- index.js
+++ index.js
@@ -23,23 +23,41 @@ function setTask(title, description) {
     })
 } 
 
+function writeTaskCard(title, description, id) {
+    let taskCard = `
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title fs-3">${title}</h5>
+                <p class="card-text fw-light">${description}</p>
+            </div>
+            <div class="card-footer d-flex justify-content-end">
+                <button 
+                    class="btn btn-primary border border-2 border-light btn-delete" 
+                    data-id="${id}"
+                >Delete</button>
+                <button 
+                    class="btn btn-secondary border border-2 border-light btn-save" 
+                    data-id="${id}"
+                >Edit</button>
+            </div>
+        </div>`;
+    return taskCard;
+}
+
 async function writeTasks() {
 
     let querySnapshot   = await firebase.firestore().collection("tasks").get();
     let tasksCards      = "";
 
     querySnapshot.forEach(doc => {
-        let data = doc.data();
-        let taskCard = `
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title fs-3">${data.title}</h5>
-                <p class="card-text fw-light">${data.description}</p>
-            </div>
-        </div>`;
-        
-        tasksCards += taskCard;
+
+        let data    = doc.data();
+            data.id = doc.id;
+
+        tasksCards += writeTaskCard(data.title, data.description, data.id);
+
     });
+
     tasksContainer.innerHTML = tasksCards;
 }
 
@@ -62,13 +80,16 @@ async function readForm() {
 taskForm.saveButton.addEventListener("click", (e) => {
     e.preventDefault();
     readForm();
-    writeTasks();
 })
 
 taskForm.deleteButton.addEventListener("click", e => {
     e.preventDefault();
     taskFormContent.reset();
 })
+
+firebase.firestore().collection("tasks").onSnapshot(() => {
+    writeTasks();
+});
 
 window.addEventListener("DOMContentLoaded", (e) => {
     writeTasks();
