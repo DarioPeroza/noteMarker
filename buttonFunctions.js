7e37bb3c56e0718db3d510b60781a486b435991c
Dario Peroza <exskey@gmail.com>
2021-03-08 23:59:20 -0400

    Edit button work

--- buttonFunctions.js
+++ buttonFunctions.js
@@ -6,8 +6,27 @@ function setBtnListeners(btnArray, callback = console.log) {
     })
 }
 
-function deleteTask(id) {
-    firebase.firestore().collection("tasks").doc(id).delete();
+async function editTask(id) {
+
+    let snapshot    = await firebase.firestore().collection("tasks").doc(id).get();
+    let title       = snapshot.data().title;
+    let description = snapshot.data().description;
+
+    taskForm.set("title", title);
+    taskForm.set("description", description);
+    taskForm.editing = true;
+    taskForm.id = id;
+
+    changeSaveButton();
+}
+
+function changeSaveButton() {
+    if (taskForm.editing) {
+        taskForm.saveButton.innerText = "Update";
+    }
+    if (!taskForm.editing) {
+        taskForm.saveButton.innerText = "Save";
+    }
 }
 
 function setupButtons() {
@@ -17,6 +36,6 @@ function setupButtons() {
         buttons.save        = document.querySelectorAll(".btn-save");
 
     setBtnListeners(buttons.delete, deleteTask);
-    setBtnListeners(buttons.save);
+    setBtnListeners(buttons.save, editTask);
 
 }
--- main.js
+++ main.js
@@ -4,9 +4,19 @@ const taskForm          = {
     description     : taskFormContent[1],
     saveButton      : taskFormContent[2],
     deleteButton    : taskFormContent[3],
+    id              : "",
+    editing         : false,
     get             : function (propiety) {
         try {
             return this[propiety].value;
+        } catch (error) {
+            return 'propiety: "' + propiety + '" not found'
+        }
+    },
+    set             : function (propiety, value) {
+        try {
+            this[propiety].value = value;
+            return value;
         } catch (error) {
             return 'propiety: "' + propiety + '" not found'
         }
@@ -22,6 +32,21 @@ function setTask(title, description) {
         description
     })
 } 
+
+async function deleteTask(id) {
+    await firebase.firestore().collection("tasks").doc(id).delete();
+}
+
+async function updateTask(id) {
+
+    let task = {
+        title       : taskForm.get("title"),
+        description : taskForm.get("description")
+    };
+
+    await firebase.firestore().collection("tasks").doc(id).update(task);
+
+}
 
 async function readForm() {
 
@@ -41,12 +66,44 @@ async function readForm() {
 //-------------------------------------------- Events --------------------------------------------
 
 taskForm.saveButton.addEventListener("click", (e) => {
+
     e.preventDefault();
-    readForm();
+
+    if (!taskForm.editing) {
+
+        readForm();
+
+    } else if (taskForm.editing) {
+
+        updateTask(taskForm.id);
+
+        taskForm.id = "";
+        taskForm.editing = false;
+
+        changeSaveButton();
+
+        taskFormContent.reset();
+        taskForm.title.focus();
+
+    }
+    
 })
 
 taskForm.deleteButton.addEventListener("click", e => {
+
     e.preventDefault();
+
+    if (taskForm.editing) {
+
+        deleteTask(taskForm.id);
+
+        taskForm.id = "";
+        taskForm.editing = false;
+
+        changeSaveButton();
+
+    }
+
     taskFormContent.reset();
 })
 
